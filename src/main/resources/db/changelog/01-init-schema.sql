f5660d4627cceb86b56e6f9952b7cda495dbba4b
Kermabon Stéphane <demis27@demis27.net>
2025-10-12 20:46:37 +0200

    Fix some sonar issue

--- src/main/resources/db/changelog/01-init-schema.sql
+++ src/main/resources/db/changelog/01-init-schema.sql
@@ -1,7 +1,7 @@
 create table countries (
     code integer not null,
-    alpha2_code varchar(2) not null,
-    alpha3_code varchar(3) not null,
-    name varchar(256) not null,
+    alpha2_code varchar2(2) not null,
+    alpha3_code varchar2(3) not null,
+    name varchar2(256) not null,
     primary key (code)
 );
