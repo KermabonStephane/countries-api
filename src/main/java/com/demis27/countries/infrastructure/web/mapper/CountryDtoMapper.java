7b6f2d0ec9fbb03f8d0ed56049113754b1600765
Kermabon Stéphane <demis27@demis27.net>
2025-11-02 21:05:09 +0100

    * [#14](https://github.com/KermabonStephane/countries-api/issues/14) Use commons-restful-spring

--- src/main/java/com/demis27/countries/infrastructure/web/mapper/CountryDtoMapper.java
+++ src/main/java/com/demis27/countries/infrastructure/web/mapper/CountryDtoMapper.java
@@ -5,6 +5,7 @@ import com.demis27.countries.domain.Region;
 import com.demis27.countries.domain.SubRegion;
 import com.demis27.countries.infrastructure.web.dto.CountryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
@@ -20,5 +21,6 @@ public interface CountryDtoMapper {
 
     Region toDomain(CountryDto.RegionDto dto);
 
+    @Mapping(target = "region", ignore = true)
     SubRegion toDomain(CountryDto.SubRegionDto dto);
 }
